a07127ac21e128fab9412c1c26e973e2f139b99c
olzhaskhamzin <khamzin@kolesa.kz>
2025-03-03 23:18:06 +0500

    Adding types to JSON object

--- app.js
+++ app.js
@@ -1,2 +1,15 @@
 "use strict";
 const a = 1;
+const office = {
+    "officeId": 45,
+    "isOpened": false,
+    "contacts": {
+        "phone": "+79100000000",
+        "email": "my@email.ru",
+        "address": {
+            "city": "Москва"
+        }
+    }
+};
+const res = JSON.stringify(office, null, 2);
+console.log(`Office data ${res}`);
--- app.ts
+++ app.ts
@@ -1,1 +1,33 @@
 const a = 1;
+
+interface Office {
+    officeId: number,
+    isOpened: boolean,
+    contacts: Contacts
+}
+
+interface Contacts {
+    phone: string,
+    email: string,
+    address: Address
+}
+
+interface Address {
+    city: string
+}
+
+const office: Office = {
+    "officeId": 45,
+    "isOpened": false,
+    "contacts": {
+        "phone": "+79100000000",
+        "email": "my@email.ru",
+        "address": {
+            "city": "Москва"
+        }
+    }
+}
+
+const res = JSON.stringify(office, null, 2);
+
+console.log(`Office data ${res}`);
