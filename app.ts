63b1b28d5c41e5446a6e543bae6986bcccd6c6af
olzhaskhamzin <khamzin@kolesa.kz>
2025-03-03 23:58:58 +0500

    Tuples

--- app.ts
+++ app.ts
@@ -1,1 +1,8 @@
-const a = 1;
+const skill:[number, string] = [1, 'Dev'];
+
+const [id, occupation] = skill;
+
+console.log(id, occupation);
+
+const occupations:[number, string, ...boolean[]] = [1, 'Dev', true, false, true];
+console.log(occupations);
